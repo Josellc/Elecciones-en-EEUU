532693e52edd7f8776f12d6f42645e25bf8cb347
Josellc <joaquinjoselcsi17@gmail.com>
2025-09-27 16:10:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -35,10 +35,9 @@ var config = {
     chapters: [
         {
             id: 'slug-style-id',
-            alignment: 'left',
+            alignment: 'fully',
             hidden: false,
             title: 'Tu voto: ¿realmente cuenta?',
-            image: './assets/video.jpeg',
             description: `
       <p>
         Imagina que vas a votar en las elecciones presidenciales de Estados Unidos y te preguntas: ¿Realmente cuenta tu voto? A continuación te explicaremos cómo sale elegido el presidente de EEUU, conocerás qué es y cómo funciona el Colegio Electoral y cómo se lleva a cabo el proceso de votación.
